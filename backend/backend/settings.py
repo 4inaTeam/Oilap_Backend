3d6d4a31411679a9add7781bd277a4164fcd6db2
Oussama Chercheri <oussama.chercheri@tek-up.de>
2025-04-13 14:37:39 +0100

    --fix bug reset-password

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'users.apps.UsersConfig',
     'rest_framework',
     'rest_framework_simplejwt',
     'django_rest_passwordreset',
--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    name = 'users'  # Keep this as-is
+
+    def ready(self):
+        # Import signals to ensure they're connected
+        import users.signals  # Add this line
